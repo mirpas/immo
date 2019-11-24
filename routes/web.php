fc894e7b95835842181a2e48d855643b5ae5dcd7
Pascal Mirring <pascal.mirring@hotelfraenkischerhof.de>
2019-11-24 22:20:01 +0100

    install laravel lteadmin3 theme

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Auth::routes();
+
+Route::get('/home', function() {
+    return view('home');
+})->name('home')->middleware('auth');
