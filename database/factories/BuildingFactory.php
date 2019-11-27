d53cd0aeead4d60945ae1bc6701693f85de506f8
Pascal Mirring <pascal.mirring@hotelfraenkischerhof.de>
2019-11-27 22:47:42 +0100

    add BuildingFactory and db:seeder to generate test data and a default superuser use: php artisan migrate:fresh --seed

---
+++ database/factories/BuildingFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Building;
+use Faker\Generator as Faker;
+
+$factory->define(Building::class, function (Faker $faker) {
+    return [
+            'short_name' => $faker->company,
+            'description' => $faker->company,
+            'street' => $faker->streetName . ' ' . $faker->buildingNumber,
+            'zip' => $faker->postcode,
+            'city' => $faker->city,
+            'flurnummer' => $faker->ean8,
+            'ground_area' => $faker->numberBetween(500, 999),
+            'building_area' => $faker->numberBetween(150, 499),
+            'year_of_construction' => $faker->year,
+            'year_of_last_renovation' => $faker->year,
+            'path_to_energieausweis' => '',
+            'building_type_id' => 1,
+
+    ];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+		DB::table('users')->insert([
+            'name' => 'Superuser',
+            'email' => 'admin@local.de',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+		// factory(App\User::class, 5)->create();
+		factory(App\Building::class, 15)->create();
     }
 }
